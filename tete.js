0b46c547dede851db2160de9730f29a7e410d215
DiegoDeSousaPereira <diego992916913@gmail.com>
2025-02-10 14:37:20 -0300

    commit

--- tete.js
+++ tete.js
@@ -4,19 +4,18 @@ function doSomething() {
     for(var i = 0; i < 10; i++) {
         console.log(i);
     }
-    console.log("Final i:", i); // i ainda é acessível aqui por causa do 'var'
+    console.log("Final i:", i); 
 }
 
 function doAnotherThing() {
     document.getElementById("myDiv").innerHTML = "<span>" + globalVar + "</span>";
-    // Manipulação direta de DOM dentro de uma função, sem separação de responsabilidades
 }
 
 doSomething();
 doAnotherThing();
 
 function manipulateData() {
-    var data = fetchData(); // fetchData não está definida em qualquer lugar
+    var data = fetchData(); 
     data.push("new data");
     console.log(data);
 }
@@ -24,12 +23,11 @@ function manipulateData() {
 manipulateData();
 
 function fetchData() {
-    // Uso ineficiente e errado de async-await, promessas não são manipuladas corretamente
     var data;
     fetch('https://api.example.com/data').then(response => {
         data = response.json();
     });
-    return data; // Retorna undefined porque fetch é assíncrono
+    return data; 
 }
 
-console.log(fetchData()); // Sempre loga undefined
+console.log(fetchData());
